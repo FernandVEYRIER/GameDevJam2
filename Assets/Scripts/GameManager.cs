4a775a7362280497f7d8d3e03fead32f60dd0407
Fernand <fernand.veyrier@epitech.eu>
2015-02-06 19:13:06 +0100

    rangemoica

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	[HideInInspector]
+	public static bool isPlaying;
+
+	public GameObject pauseCanvas;
+
+
+	void Start () 
+	{
+		isPlaying = true;
+
+	}
+	
+
+	void Update () 
+	{
+		//handles pause
+		if (Input.GetButtonDown("Pause"))
+		{
+			isPlaying = !isPlaying;
+			pauseCanvas.SetActive(!pauseCanvas.activeSelf);
+			Time.timeScale = (Time.time == 1) ? 0 : 1;
+		}
+	}
+}
