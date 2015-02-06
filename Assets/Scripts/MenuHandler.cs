c8658e4e97f24e5f640b60a1acc2e27b8582a315
Fernand <fernand.veyrier@epitech.eu>
2015-02-06 15:34:52 +0100

    Flammes !

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuHandler : MonoBehaviour {
+
+	public void OnPlayClick()
+	{
+		Application.LoadLevel(1);
+	}
+
+	public void OnQuitClick()
+	{
+		Application.Quit();
+	}
+}
