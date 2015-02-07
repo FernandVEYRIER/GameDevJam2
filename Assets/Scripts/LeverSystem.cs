8a0bc29daf9d06d4dfec9f7d3f58f8f44f94ffaa
Fernand <fernand.veyrier@epitech.eu>
2015-02-07 20:08:42 +0100

    letoto

---
+++ Assets/Scripts/LeverSystem.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider2D))]
+public class LeverSystem : MonoBehaviour {
+
+	public Sprite leverSpriteOff;
+	public Sprite leverSpriteOn;
+
+	public GameObject [] torch;
+
+	public KeyCode key;
+	bool areActive = false;
+
+	void Start () 
+	{
+		this.GetComponent<SpriteRenderer>().sprite = leverSpriteOff;
+		foreach (GameObject _torch in torch)
+		{
+			_torch.SetActive(false);
+		}
+	}
+	
+	void OnTriggerStay2D(Collider2D col)
+	{
+		if (Input.GetKeyDown(key))
+		{
+			areActive = !areActive;
+			if (areActive)
+			{
+				this.GetComponent<SpriteRenderer>().sprite = leverSpriteOn;
+				foreach (GameObject _torch in torch)
+				{
+					_torch.SetActive(true);
+					_torch.GetComponent<TorchScript>().startAnim();
+				}
+			}
+			else
+			{
+				this.GetComponent<SpriteRenderer>().sprite = leverSpriteOff;
+				foreach (GameObject _torch in torch)
+				{
+					_torch.SetActive(false);
+				}
+			}
+		}
+	}
+}
--- Assets/Scripts/TorchScript.cs
+++ Assets/Scripts/TorchScript.cs
@@ -3,23 +3,28 @@ using System.Collections;
 
 public class TorchScript : MonoBehaviour {
 
-	public float maxAngle = 60;
-	public float minAngle = 58;
+	//public float maxAngle = 60;
+	//public float minAngle = 58;
 
 	public float minIntensity = 0.8f;
 	public float maxIntensity = 1;
 
-	void Start () 
+	void Start() 
 	{
 		StartCoroutine(simulateTorch());
 	}
-	
+
+	public void startAnim()
+	{
+		StartCoroutine(simulateTorch());
+	}
+
 	IEnumerator simulateTorch()
 	{
 		while (true)
 		{
 			this.GetComponent<Light>().intensity = Random.Range(minIntensity, maxIntensity);
-			this.GetComponent<Light>().spotAngle = Random.Range(minAngle, maxAngle);
+			//this.GetComponent<Light>().spotAngle = Random.Range(minAngle, maxAngle);
 			yield return new WaitForSeconds(0.1f);
 		}
 	}
