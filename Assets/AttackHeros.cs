1cf4b670b8bfe1a5c13d06c91a0afc69989d34cb
Nicolas-Constanty <nicolas.constanty@epitech.eu>
2015-02-07 23:02:47 +0100

    aie

---
+++ Assets/AttackHeros.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackHeros : MonoBehaviour {
+
+	public Transform	spawnpoint;
+	public GameObject	projectile;
+	public float		speed;
+	private bool		activate = false;
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Space) && activate == false)
+		{
+			StartCoroutine(timer ());
+			GameObject obj = (GameObject)Instantiate (projectile, spawnpoint.position, spawnpoint.rotation);
+			obj.transform.localScale = transform.localScale;
+			obj.rigidbody2D.velocity = new Vector2(speed, 0f) * transform.localScale.x;
+		}
+	}
+	IEnumerator		timer()
+	{
+		activate = true;
+		yield return new WaitForSeconds (1);
+		activate = false;
+	}
+}
---
+++ Assets/DestroyProjectile.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyProjectile : MonoBehaviour {
+	
+	// Use this for initialization
+	void Awake ()
+	{
+		Destroy (gameObject, 0.7f);
+	}
+}
