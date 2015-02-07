2d3d4a5a55dda976d8bd4be09bdd891d78d04174
Fernand <fernand.veyrier@epitech.eu>
2015-02-07 21:34:49 +0100

    alamano

---
+++ Assets/Scripts/LeverDoorSystem.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeverDoorSystem : MonoBehaviour {
+
+	public GameObject door;	
+	public KeyCode key;
+
+	void OnTriggerStay2D(Collider2D col)
+	{
+		if (Input.GetKeyUp(key))
+		{
+			//change door state
+			Debug.Log("Door");
+		}
+	}
+}
