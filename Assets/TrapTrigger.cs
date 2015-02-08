0642e43091d497e4a6f172871cdb193bf9bfca2e
Fernand <fernand.veyrier@epitech.eu>
2015-02-08 07:42:30 +0100

    letoto

---
+++ Assets/TrapTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapTrigger : MonoBehaviour {
+
+	Animator anim;
+	// Use this for initialization
+	void Start () 
+	{
+		anim = this.GetComponent<Animator>();
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.collider.tag == "Player")
+		{
+			if (anim.GetBool("isOpen") == false)
+				anim.SetBool("isOpen", true);
+			Invoke("closeTrap", 1f);
+		}
+	}
+
+	void closeTrap()
+	{
+		anim.SetBool("isOpen", false);
+	}
+}
