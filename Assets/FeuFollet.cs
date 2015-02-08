4543ebe86c185b853c74e70f6cfc9eab2ab53d07
Nicolas-Constanty <nicolas.constanty@epitech.eu>
2015-02-08 04:09:56 +0100

    The Hell

---
+++ Assets/FeuFollet.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FeuFollet : MonoBehaviour {
+
+	public	bool	activate = true;
+	private Vector3 save;
+	// Use this for initialization
+	void Start ()
+	{
+		save = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (activate == true)
+		{
+			Screen.showCursor = false;
+			transform.position = new Vector3 (Camera.main.ScreenToWorldPoint (Input.mousePosition).x, Camera.main.ScreenToWorldPoint (Input.mousePosition).y, 0f);
+		} 
+		else
+			Screen.showCursor = true;
+	}
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.tag != "Player")
+			Destroy (gameObject);
+	}
+}
---
+++ Assets/LeverBlue.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeverBlue : MonoBehaviour {
+
+	public Sprite	levier_on;
+	public Sprite	levier_off;
+	public GameObject	luciole;
+	private bool	activate = false;
+	private GameObject obj = null;
+	void Update()
+	{
+		if (activate)
+		{
+			if (obj == null)
+				obj = (GameObject)Instantiate(luciole, transform.position, transform.rotation);
+			obj.transform.position = Vector3.MoveTowards (obj.transform.position, Camera.main.ScreenToWorldPoint(new Vector3(40, Screen.height - 40, 1)), 0.1f);
+		}
+	}
+	void OnTriggerStay2D(Collider2D col)
+	{
+		if (col.tag == "Player" && Input.GetKeyDown (KeyCode.E))
+		{
+			activate = !activate;
+		}
+		if (activate)
+			gameObject.GetComponent<SpriteRenderer> ().sprite = levier_on;
+		else
+			gameObject.GetComponent<SpriteRenderer> ().sprite = levier_off;
+	}
+}
