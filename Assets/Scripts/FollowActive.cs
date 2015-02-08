a0e8bd147d0f71477787bbe10692ade0922695a3
Nicolas-Constanty <nicolas.constanty@epitech.eu>
2015-02-08 12:03:15 +0100

    Enfin

--- Assets/Scripts/FollowActive.cs
+++ Assets/Scripts/FollowActive.cs
@@ -10,30 +10,35 @@ public class FollowActive : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
-		player = GameObject.Find(players [index]);
+		player = GameObject.FindGameObjectWithTag(players [index]);
 	}
 
 	// Update is called once per frame
 	void Update () 
 	{
+		//player = GameObject.FindGameObjectWithTag(players[index]);
+		Debug.Log (player);
+
 		if (Input.GetKeyDown(KeyCode.Tab))
 		{
-			if (GameObject.Find("shotBub(Clone)") != null)
+			if (GameObject.FindGameObjectWithTag("Feufollet") != null)
 				index++;
 			if (index >= players.Length)
 				index = 0;
-			if (players[index] != null)
-				player = GameObject.Find(players[index]);
-			if (GameObject.Find("shotBub(Clone)") != null && player.name == "shotBub(Clone)" && Input.GetKeyDown(KeyCode.Tab))
+			//if (players[index] != null)
+			player = GameObject.FindGameObjectWithTag(players[index]);
+			if (GameObject.FindGameObjectWithTag("Feufollet") != null && player.name == "shotBub(Clone)" && Input.GetKeyDown(KeyCode.Tab))
 			{
 				player.collider2D.GetComponent<FeuFollet>().activate = true;
 			}
-			else if (GameObject.Find("shotBub(Clone)") != null)
-				GameObject.Find("shotBub(Clone)").GetComponent<FeuFollet>().activate = false;
+			else if (GameObject.FindGameObjectWithTag("Feufollet") != null)
+				GameObject.FindGameObjectWithTag("Feufollet").GetComponent<FeuFollet>().activate = false;
 		}
-		if (player != GameObject.Find("shotBub(Clone)"))
+		//if (player == null)
+		//	return;
+		if (player != GameObject.FindGameObjectWithTag("Feufollet"))
 			this.transform.position = Vector3.SmoothDamp(this.transform.position, player.transform.position + new Vector3(0, 0, -10), ref vel, 0.25f);
-		if (player == GameObject.Find("shotBub(Clone)") && player.GetComponent<FeuFollet>().time == true)
+		if (player == GameObject.FindGameObjectWithTag("Feufollet") && player.GetComponent<FeuFollet>().time == true)
 			this.transform.position = Vector3.SmoothDamp(this.transform.position, player.transform.position + new Vector3(0, 0, -10), ref vel, 0.25f);
 	}
 }
