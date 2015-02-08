9f957d6b5e2d4dfc6678c30c9f9c7dbb2bafcc6f
Fernand <fernand.veyrier@epitech.eu>
2015-02-08 04:28:32 +0100

    letoto

---
+++ Assets/Scripts/PicsKill.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PicsKill : MonoBehaviour {
+
+	public GameObject gameManager;
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		Debug.Log(col);
+		if (col.collider2D.tag == "Player")
+			gameManager.GetComponent<GameManager>().playerDeath(col.collider2D.gameObject);
+	}
+}
