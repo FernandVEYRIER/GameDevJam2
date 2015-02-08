38001593a75d401fc78cad3e92a00e7c767a840a
Fernand <fernand.veyrier@epitech.eu>
2015-02-08 01:58:06 +0100

    letoto

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class FollowPlayer : MonoBehaviour {
 
 	GameObject player;
+	Vector3 vel;
 	// Use this for initialization
 	void Start () 
 	{
@@ -18,6 +19,7 @@ public class FollowPlayer : MonoBehaviour {
 			player = GameObject.FindGameObjectWithTag("Player");
 			return;
 		}
-		this.transform.position = player.transform.position + new Vector3(0, 0, -10);
+		this.transform.position = Vector3.SmoothDamp(this.transform.position, player.transform.position + new Vector3(0, 0, -10), ref vel, 0.25f);
+		//this.transform.position = player.transform.position + new Vector3(0, 0, -10);
 	}
 }
