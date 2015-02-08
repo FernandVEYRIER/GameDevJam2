0b8d966d2e888b1fd2c8f0f157444e42cf8c35a0
Nicolas-Constanty <nicolas.constanty@epitech.eu>
2015-02-08 07:41:34 +0100

    fu

--- Assets/FeuFollet.cs
+++ Assets/FeuFollet.cs
@@ -4,6 +4,11 @@ using System.Collections;
 public class FeuFollet : MonoBehaviour {
 
 	public	bool	activate = true;
+	[HideInInspector]
+	public bool	time = false;
+	private bool	time1 = false;
+	private bool	mouse = false;
+	public Transform lum;
 	private Vector3 save;
 	// Use this for initialization
 	void Start ()
@@ -17,14 +22,48 @@ public class FeuFollet : MonoBehaviour {
 		if (activate == true)
 		{
 			Screen.showCursor = false;
-			transform.position = new Vector3 (Camera.main.ScreenToWorldPoint (Input.mousePosition).x, Camera.main.ScreenToWorldPoint (Input.mousePosition).y, 0f);
+			if (transform.position == GameObject.Find("SpawLum").transform.position && time == false && mouse == true)
+				time = true;
+			SpawPos();
 		} 
 		else
+		{
 			Screen.showCursor = true;
+			time1 =false;
+			initpos();
+		}
+		if (time == true)
+		{
+			transform.position = new Vector3 (Camera.main.ScreenToWorldPoint (Input.mousePosition).x, Camera.main.ScreenToWorldPoint (Input.mousePosition).y, 0f);
+			transform.position = Vector3.MoveTowards (transform.position, lum.position, 0.1f);
+		}
+	}
+	public void initpos()
+	{
+		transform.position = Vector3.MoveTowards (transform.position, Camera.main.ScreenToWorldPoint(new Vector3(40, Screen.height - 40, 1)), 0.1f);
+	}
+	public void SpawPos()
+	{
+		transform.position = Vector3.MoveTowards (transform.position, GameObject.Find("SpawLum").transform.position, 0.1f);
 	}
 	void OnTriggerEnter2D(Collider2D col)
 	{
-		if (col.tag != "Player")
-			Destroy (gameObject);
+		if (col.tag != "Player" && activate == true)
+		{
+			time = false;
+			activate = true;
+			mouse = false;
+			transform.position = GameObject.Find("SpawLum").transform.position;
+		}
+	}
+	void OnMouseEnter()
+	{
+		mouse = true;
+	}
+	IEnumerator		timer()
+	{
+		time = false;
+		yield return new WaitForSeconds (1);
+		time = true;
 	}
 }
---
+++ Assets/FollowActive.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowActive : MonoBehaviour {
+	
+	public string[] players;
+	private int			index = 0;
+	private GameObject	player;
+	Vector3 vel;
+	// Use this for initialization
+	void Start () 
+	{
+		player = GameObject.Find(players [index]);
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.Tab))
+		{
+			if (GameObject.Find("shotBub(Clone)") != null)
+				index++;
+			if (index == players.Length)
+				index = 0;
+			if (players[index] != null)
+				player = GameObject.Find(players[index]);
+			if ( player.name == "shotBub(Clone)" && Input.GetKeyDown(KeyCode.Tab))
+				player.GetComponent<FeuFollet>().activate = true;
+			else if (GameObject.Find("shotBub(Clone)") != null)
+				GameObject.Find("shotBub(Clone)").GetComponent<FeuFollet>().activate = false;
+		}
+		if (player != GameObject.Find("shotBub(Clone)"))
+			this.transform.position = Vector3.SmoothDamp(this.transform.position, player.transform.position + new Vector3(0, 0, -10), ref vel, 0.25f);
+		if (player == GameObject.Find("shotBub(Clone)") && player.GetComponent<FeuFollet>().time == true)
+			this.transform.position = Vector3.SmoothDamp(this.transform.position, player.transform.position + new Vector3(0, 0, -10), ref vel, 0.25f);
+	}
+}
